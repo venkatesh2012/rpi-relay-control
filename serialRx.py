f1b83b03014e3d2c82068c2dd864ab74658b170c
Venkatesh Srinivasan <22662559+venkatesh2012@users.noreply.github.com>
2022-09-25 20:11:24 +0200

    add cleanup code

--- serialRx.py
+++ serialRx.py
@@ -1,5 +1,11 @@
 from time import sleep
+import logging
+# from systemd.journal import JournalHandler
 import serial
+import RPi.GPIO as GPIO
+log = logging.getLogger('uartRx')
+#log.addHandler(JournalHandler())
+log.setLevel(logging.INFO)
 ser = serial.Serial("/dev/ttyS0", baudrate = 9600, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE, bytesize=serial.EIGHTBITS, timeout=1)
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
@@ -13,25 +19,32 @@ GPIO.setup(rChannel2,GPIO.OUT)
 GPIO.setup(rChannel3,GPIO.OUT)
 rChannel=0
 rChannelValue=GPIO.LOW
-while 1:
-  rx_data = ser.read()
-  sleep(0.03)
-  data_left = ser.inWaiting()
-  rx_data += ser.read(data_left)
-  rxString=rx_data.decode().strip()
-  if rxString :
-    relay=rxString.split("Relay: ")[1].split("-")
-    if relay[1]=="0":
-      rChannelValue=GPIO.LOW
-    else:
-      rChannelValue=GPIO.HIGH
-    if relay[0]=="0":
-      rChannel=rChannel0
-    elif relay[0]=="1":
-      rChannel=rChannel1
-    elif relay[0]=="2":
-      rChannel=rChannel2
-    elif relay[0]=="3":
-      rChannel=rChannel3;
-    print(rChannel,rChannelValue)
-    GPIO.output(rChannel, rChannelValue)
+try:
+  while 1:
+    rx_data = ser.read()
+    sleep(0.03)
+    data_left = ser.inWaiting()
+    rx_data += ser.read(data_left)
+    rxString=rx_data.decode().strip()
+    if rxString :
+      relay=rxString.split("Relay: ")[1].split("-")
+      if relay[1]=="0":
+        rChannelValue=GPIO.LOW
+      else:
+        rChannelValue=GPIO.HIGH
+      if relay[0]=="0":
+        rChannel=rChannel0
+      elif relay[0]=="1":
+        rChannel=rChannel1
+      elif relay[0]=="2":
+        rChannel=rChannel2
+      elif relay[0]=="3":
+        rChannel=rChannel3;
+      print(rChannel,rChannelValue)
+      log.info(rChannel, rChannelValue)
+      GPIO.output(rChannel,rChannelValue)
+except KeyboardInterrupt:
+    print("Stopping serialRx")
+    log.info("Stopping SerialRx")
+    ser.close()
+    GPIO.cleanup()
